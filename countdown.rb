5c79624e425d45dd76d059b3b72c731361c1e79c
Larry Hawelu <lhawelu@gmail.com>
2021-04-05 16:17:24 -0700

    commit tree 739797ea0810331f75e5dc63a6d3cbfa65901271

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,21 @@
 #write your code here
 
-def countdown
-end
+def countdown timer
+  while timer > 0
+    number = timer
+    timer -= 1
+    puts "#{number} SECOND(S)!"
+  end
+  "HAPPY NEW YEAR!"
+end 
+
+def countdown_with_sleep timer
+  while timer > 0
+    sleep 1
+    number = timer
+    timer -= 1
+    puts "#{number} SECOND(S)!"
+  end
+  "HAPPY NEW YEAR!"
+end 
+  
